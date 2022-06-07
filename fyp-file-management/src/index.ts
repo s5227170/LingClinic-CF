e108beb396b4100406662f0c6bc2c6bc19950cca
s5227170 <Hri100_96@abv.bg>
2022-06-07 17:35:52 +0100

    Added Helmet protection. Added DOS/DDOS prevention middleware (rate limiter)

--- fyp-file-management/src/index.ts
+++ fyp-file-management/src/index.ts
@@ -2,9 +2,10 @@ import cors from "cors";
 import express, { Application, NextFunction, Request, Response } from "express";
 import CustomError from "./models/CustomError";
 import decodeToken from "./middleware/decoder";
-import appointmentRoutes from "./routes/index";
 import fileRoutes from "./routes/index";
 import dbConnection from "./middleware/db";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 const fileMiddleware = require("express-multipart-file-parser");
 
@@ -20,6 +21,17 @@ if (process.env.NODE_ENV == "production") {
 } else {
   corsOptions.origin = "*";
 }
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 240, // Limit each IP to 240 requests per `window` (here, per 1 minute, or 4 requests a second)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(limiter);
+
+app.use(helmet());
 
 app.use(cors(corsOptions));
 
--- fyp-schedules/src/index.ts
+++ fyp-schedules/src/index.ts
@@ -4,6 +4,8 @@ import CustomError from "./models/CustomError";
 import decodeToken from "./middleware/decoder";
 import scheduleRoutes from "./routes/index";
 import dbConnection from "./middleware/db";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 const fileMiddleware = require("express-multipart-file-parser");
 
@@ -19,6 +21,17 @@ if (process.env.NODE_ENV == "production") {
 } else {
   corsOptions.origin = "*";
 }
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 240, // Limit each IP to 240 requests per `window` (here, per 1 minute, or 4 requests a second)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(limiter);
+
+app.use(helmet());
 
 app.use(cors(corsOptions));
 
